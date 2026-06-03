c532cf516625b9a2d2cb1d7055d11b9e770d9a2c
MOUNIKA CHEEKATLA <mounikacheekatla422@gmail.com>
2026-06-03 19:46:02 +0530

    Create Three Divisors.py

---
+++ Three Divisors.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        arr=[]
+        for i in range(1,n+1):
+            if(n%i==0):
+                arr.append(i)
+        if(len(arr)==3):
+            return True
+        else:
+            return False
