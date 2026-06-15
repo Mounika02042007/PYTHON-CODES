8732fec7daa814e60703a21f1806ca1c041e34c8
MOUNIKA CHEEKATLA <mounikacheekatla422@gmail.com>
2026-06-15 15:09:50 +0530

    Create Alternates in Array.py

---
+++ Alternates in Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getAlternates(self, arr):
+        take=[]
+        for i in range(0,len(arr)):
+            if i%2==0:
+                take.append(arr[i])
+        return tak
